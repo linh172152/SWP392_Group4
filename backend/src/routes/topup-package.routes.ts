8ce56aea41a76323443dde2f19b6f0c919e0b36e
Lê Đức Thành <125138016+LeDucThanhh@users.noreply.github.com>
2025-11-07 21:14:36 +0700

    fix swagger

--- backend/src/routes/topup-package.routes.ts
+++ backend/src/routes/topup-package.routes.ts
@@ -30,7 +30,7 @@ router.use(authorizeRole("ADMIN"));
  *         name: is_active
  *         schema:
  *           type: boolean
- *         description: "Filter by active status (default true)"
+ *         description: Filter by active status
  *       - in: query
  *         name: page
  *         schema:
